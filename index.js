5af5a9c996e5c92bc96aaf702f87789e5ddb1d34
Andrey-Kharebov <50665053+Andrey-Kharebov@users.noreply.github.com>
2022-03-10 18:40:09 +0300

    Added CORS settings

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express()
 
 app.use(bodyParser.json())
 
+// CORS settings
+app.use((req, res, next) => { 
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9001') // '*'
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept') // '*'
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  next()
+})
+
 mongoose
   .connect('mongodb+srv://Andrey:w4oHzEqQbJlQZqN5@library-app-cluster.ojgsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then(() => {
